eb275c9b0579dfcb737451243d25cca777446001
Biman Pakhira <bimanpakhira2@gmail.com>
2026-05-22 09:12:42 +0530

    fix: use Express 5 wildcard syntax

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,13 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 const platformRoutes = require('./routes/authAndJobs');
 app.use('/api', platformRoutes);
 
-app.get('*', (req, res) => {
+// Fix for Express 5
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
